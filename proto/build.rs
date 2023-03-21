cd087dde36813dee4277e28d5a93f706f3a105bd
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-03-21 14:47:18 +0100

    build(proto): use tenderdash-proto-compiler as lib, not binary

--- proto/build.rs
+++ proto/build.rs
@@ -1,26 +1,13 @@
-use std::{
-    env,
-    path::Path,
-    process::{exit, Command},
-};
+use std::{env, path::Path};
 
 fn main() {
     let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let version = env!("CARGO_PKG_VERSION");
-    let proto_compiler_dir = manifest_dir.join("..").join("tools").join("proto-compiler");
+    // let proto_compiler_dir =
+    // manifest_dir.join("..").join("tools").join("proto-compiler");
 
-    let output = Command::new("cargo")
-        .current_dir(proto_compiler_dir)
-        .env("TENDERDASH_COMMITISH", "v".to_owned() + version)
-        .arg("run")
-        .output()
-        .expect("failed to generate protobuf files with Cargo");
-
-    if !output.status.success() {
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-        exit(output.status.code().unwrap());
-    }
+    env::set_var("TENDERDASH_COMMITISH", "v".to_owned() + version);
+    tenderdash_proto_compiler::proto_compile().expect("protobuf definitions compilation failed");
 
     println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
 }
--- tools/proto-compiler/src/main.rs
+++ tools/proto-compiler/src/main.rs
@@ -10,80 +10,8 @@ use functions::{
 
 mod constants;
 use constants::{CUSTOM_FIELD_ATTRIBUTES, CUSTOM_TYPE_ATTRIBUTES, TENDERDASH_REPO};
+use tenderdash_proto_compiler::proto_compile;
 
 fn main() {
-    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let tenderdash_lib_target = root
-        .join("..")
-        .join("..")
-        .join("proto")
-        .join("src")
-        .join("tenderdash.rs");
-    let target_dir = root
-        .join("..")
-        .join("..")
-        .join("proto")
-        .join("src")
-        .join("prost");
-    let out_dir = var("OUT_DIR")
-        .map(PathBuf::from)
-        .or_else(|_| tempdir().map(|d| d.into_path()))
-        .unwrap();
-    let tenderdash_dir = PathBuf::from(var("TENDERDASH_DIR").unwrap_or_else(|_| {
-        root.join("..")
-            .join("target")
-            .join("tenderdash")
-            .to_str()
-            .unwrap()
-            .to_string()
-    }));
-
-    let thirdparty_dir = root.join("third_party");
-
-    let commitish = tenderdash_commitish();
-    println!("[info] => Fetching {TENDERDASH_REPO} at {commitish} into {tenderdash_dir:?}");
-    fetch_commitish(&PathBuf::from(&tenderdash_dir), TENDERDASH_REPO, &commitish); // This panics if it fails.
-
-    let proto_paths = vec![tenderdash_dir.join("proto").join("tendermint").join("abci")];
-    let proto_includes_paths = vec![tenderdash_dir.join("proto"), thirdparty_dir];
-    // List available proto files
-    let protos = find_proto_files(proto_paths);
-
-    let mut pb = prost_build::Config::new();
-
-    // Use shared Bytes buffers for ABCI messages:
-    pb.bytes([".tenderdash.abci"]);
-
-    // Compile proto files with added annotations, exchange prost_types to our own
-    pb.out_dir(&out_dir);
-    for type_attribute in CUSTOM_TYPE_ATTRIBUTES {
-        pb.type_attribute(type_attribute.0, type_attribute.1);
-    }
-    for field_attribute in CUSTOM_FIELD_ATTRIBUTES {
-        pb.field_attribute(field_attribute.0, field_attribute.1);
-    }
-    // The below in-place path redirection replaces references to the Duration
-    // and Timestamp WKTs with our own versions that have valid doctest comments.
-    // See also https://github.com/danburkert/prost/issues/374 .
-    pb.extern_path(
-        ".google.protobuf.Duration",
-        "super::super::google::protobuf::Duration",
-    );
-    pb.extern_path(
-        ".google.protobuf.Timestamp",
-        "super::super::google::protobuf::Timestamp",
-    );
-
-    println!("[info] => Determining ABCI protocol version.");
-    let abci_ver = abci_version(tenderdash_dir);
-
-    println!("[info] => Creating structs.");
-    pb.compile_protos(&protos, &proto_includes_paths).unwrap();
-
-    println!("[info] => Removing old structs and copying new structs.");
-    copy_files(&out_dir, &target_dir); // This panics if it fails.
-
-    generate_tenderdash_lib(&out_dir, &tenderdash_lib_target, &abci_ver);
-
-    println!("[info] => Done!");
+    proto_compile().expect("protobuf definitions compilation failed")
 }

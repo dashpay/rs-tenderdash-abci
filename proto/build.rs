7b440770441acde00e4918b491f5aaad19541c80
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-03-22 13:40:13 +0100

    fix(proto): fix ineffective code caused by replace of tendermint with tenderdash

--- proto/build.rs
+++ proto/build.rs
@@ -1,10 +1,7 @@
-use std::{env, path::Path};
+use std::env;
 
 fn main() {
-    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let version = env!("CARGO_PKG_VERSION");
-    // let proto_compiler_dir =
-    // manifest_dir.join("..").join("tools").join("proto-compiler");
 
     env::set_var("TENDERDASH_COMMITISH", "v".to_owned() + version);
     tenderdash_proto_compiler::proto_compile().expect("protobuf definitions compilation failed");

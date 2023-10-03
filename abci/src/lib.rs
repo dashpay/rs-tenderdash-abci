4a5df52faf03800712cd0d837279cbc3a96eb070
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-10-03 15:39:26 +0200

    fix(tenderdash-abci): correct handling of features

--- abci/src/lib.rs
+++ abci/src/lib.rs
@@ -21,6 +21,7 @@ use std::io;
 pub use application::{check_version, Application, RequestDispatcher};
 use prost::{DecodeError, EncodeError};
 #[allow(deprecated)]
+#[cfg(feature = "server")]
 pub use server::{start_server, CancellationToken, Server, ServerBuilder};
 pub use tenderdash_proto as proto;
 

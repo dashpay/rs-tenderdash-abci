3bf8d72576218c347d81da7340fe5852c8b62518
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2024-02-21 10:28:10 +0100

    chore(proto): fix imports

--- proto/src/error.rs
+++ proto/src/error.rs
@@ -1,7 +1,9 @@
 //! This module defines the various errors that be raised during Protobuf
 //! conversions.
-
+#[cfg(no_std)]
 use core::{convert::TryFrom, fmt::Display, num::TryFromIntError};
+#[cfg(not(no_std))]
+use std::{fmt::Display, num::TryFromIntError};
 
 use flex_error::{define_error, DisplayOnly};
 use prost::{DecodeError, EncodeError};
